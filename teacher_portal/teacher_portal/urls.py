92f1f02648e36e29844be8b821f3b1ea30930d34
Abhishek Kumar <abhishek1py@gmail.com>
2025-08-24 10:40:29 +0530

    Run collectstatic and added urls settings to run debug false with static files

--- teacher_portal/teacher_portal/urls.py
+++ teacher_portal/teacher_portal/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('management.urls')),
+]
+
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
