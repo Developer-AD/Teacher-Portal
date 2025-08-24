18a152ac75cac16d3727692cb65f7269e622e400
Abhishek Kumar <abhishek1py@gmail.com>
2025-08-24 10:34:46 +0530

    Run project with Docker no static file

--- teacher_portal/teacher_portal/settings.py
+++ teacher_portal/teacher_portal/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_ao1@oj*l-1zl_7_%!sql-t@@)9rq4io8ggzur^uyqp83&o1kj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
